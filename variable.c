f999093f6f36a89d5107c7d0fc12f023b689dcaa
saniyaJShaikh <sanu2shaikh2005@gmail.com>
2024-11-03 07:32:46 +0530

    variable program

---
+++ variable.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int stdid=23;
+	int stdrolno=66;
+	float stdpercentage=83.2;
+	char stdname='s';
+	printf("student id is:%d\n",stdid);
+	printf("student roll no. is:%d\n",stdrolno);
+	printf("student percentage is:%f\n",stdpercentage);
+	printf("student name is:%c\n",stdname);
+	return 0;
+}
+**output**
+student id is:23
+student roll no. is:66
+student percentage is:83.199997
+student name is:s
+
